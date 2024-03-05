63dd94d5891dfb7a06d0a3887db47dd8849e0427
Robel <robel.salgado@unah.hn>
2024-03-05 17:32:51 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const campoTexto=document.getElementsByClassName("area-igresar-texto");
+const campoMensaje=document.getElementsByClassName("text-area-mensaje");
+
+const matrizCode=[
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
+
+console.log(campoTexto.value)
